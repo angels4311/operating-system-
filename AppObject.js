7895b6fb7fc2cab753ba82076d7a26ef021af102
angels4311 <8asoto2@stpatrick.org>
2016-10-12 11:28:17 -0500

    Add files via upload
    Made some changes in the app

---
+++ AppObject.js
@@ -0,0 +1,34 @@
+function App(lights out){
+
+    //Declare your instance variables here.
+    this.strength
+    this.stamina
+    this.weight
+
+    //Declare the instance function open here.
+    this.open = function(lights out){
+
+
+
+    };
+    //Declare the instance function sleep here.
+
+
+
+    //Declare the instance function active here.
+
+
+
+    //Declare the instance function close here.
+
+
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.
